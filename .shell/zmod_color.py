7fe48cde1e09cba45bf1b1f62e8167796b074594
Sergei Rozhkov <zmod@zmod.link>
2026-09-06 11:21:31 +0500

    Z

--- .shell/zmod_color.py
+++ .shell/zmod_color.py
@@ -875,7 +875,7 @@ class zmod_color:
         saved_gcode_y = saved_last_position[1] - saved_base_position[1]
         saved_gcode_z = saved_last_position[2] - saved_base_position[2]
 
-        self.gcode.run_script_from_command(f"G1 X{saved_gcode_x:.3f} Y{saved_gcode_y:.3f} F6000\nM400\nG1 Z{saved_gcode_x:.3f}\nM400")
+        self.gcode.run_script_from_command(f"G1 X{saved_gcode_x:.3f} Y{saved_gcode_y:.3f} F6000\nM400\nG1 Z{saved_gcode_z:.3f} F6000\nM400")
 
         # Восстановление физических координат
         self.gcode.run_script_from_command("RESTORE_GCODE_STATE NAME=_T_TOOL_STATE MOVE=1 MOVE_SPEED=100")
